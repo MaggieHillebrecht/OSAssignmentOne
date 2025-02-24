a3e2e6ae48f682c40b5933fe3a473f984eefea78
Joel Escobar <joelescobar0404@gmail.com>
2025-02-24 14:46:52 -0800

    Read() fixes

--- fops_test.c
+++ fops_test.c
@@ -1,7 +1,4 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/syscall.h>
 #include <errno.h>
 #include <string.h>
 #include "fops.h"
@@ -19,72 +16,46 @@ int main(int argc, char** argv){
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
-        }
+        } else {
         createFile(argv[2], (O_CREAT | O_WRONLY | O_TRUNC), (0744));
+        }
     }
     else if (strcmp(argv[1], "write") == 0) {
         if (argc != 4) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
-        }
-        // writeToFile(argv[2], argv[3], 1);
+        } else {
         writeToFile(createFile(argv[2], (O_WRONLY), 644), argv[3], strlen(argv[3]));
+        strerror(errno);
+        }
     }
     else if (strcmp(argv[1], "read") == 0) {
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
-        }
-        // readFromFile(argv[2], "", 1);
+        } else {
         char buffer[500];
         readFromFile(createFile(argv[2],O_RDONLY, 444), buffer, 5000);
         printf("%s\n", buffer);
+        }
     }
     else if (strcmp(argv[1], "close") == 0) {
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
+        } else {
+        closeFile(createFile(argv[2],O_RDONLY, 444));
         }
-        closeFile(__O_CLOEXEC);
-        // closeFile(argv[2]);
     }
     else if (strcmp(argv[1], "delete") == 0) {
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
-        }
+        } else {
         deleteFile(argv[2]);
+        }
     }
     else {
-        strerror(errno);
+        perror("Invalid command");
     }
-
-    /*
-   switch (argv[1])
-   {
-   case 'create':
-    createFile(argv[2], 1, open);
-    break;
-
-    case 'write':
-        writeToFile(argv[2], argv[3], 1);
-        break;
-
-    case 'read':
-        readFromFile(argv[2], buf, 1);
-        break;
-
-    case 'close':
-        closeFile(argv[2]);
-        break;
-
-    case 'delete':
-        deleteFile(argv[2]);
-        break;
-   
-   default:
-        strerror(errno);
-        break;
-   }
-        */
 }
