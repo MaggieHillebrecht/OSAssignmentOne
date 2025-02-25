afbfb1237c9062f9deb667da883b858bf656e071
Maggie Hillebrecht <hillebrecht@chapman.edu>
2025-02-24 22:04:11 -0800

    pushing final assignment

--- fops_test.c
+++ fops_test.c
@@ -17,6 +17,7 @@ int main(int argc, char** argv){
             return -1;
         } else {
             result = createFile(argv[2], (O_CREAT | O_WRONLY | O_EXCL), (0744));
+            /*Open() returns a nonnegative number if successful so checks for -1 if failed*/
             if (result < 0) {
                 perror("Error creating file");
                 return -1;
@@ -24,11 +25,13 @@ int main(int argc, char** argv){
         }
     }
     else if (strcmp(argv[1], "write") == 0) {
+        /*Checks correct arg amount and then checks if the syscall was successful*/
         if (argc != 4) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
         } else {
             result = writeToFile(createFile(argv[2], (O_WRONLY), 644), argv[3], strlen(argv[3]));
+            /*Write() returns the number of bytes written so checks for -1 which means syscall failed*/
             if (result < 0) {
                 perror("Error writing to file");
                 return -1;
@@ -36,12 +39,14 @@ int main(int argc, char** argv){
         }
     }
     else if (strcmp(argv[1], "read") == 0) {
+        /*Checks correct arg amount and then checks if the syscall was successful*/
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
         } else {
             char buffer[500];
             result = readFromFile(createFile(argv[2],O_RDONLY, 444), buffer, sizeof(buffer));
+            /*Read() returns the number of bytes read so checks for -1 which means syscall failed*/
             if (result < 0) {
                 perror("Error reading from file");
                 return -1;
@@ -52,11 +57,13 @@ int main(int argc, char** argv){
         }
     }
     else if (strcmp(argv[1], "close") == 0) {
+        /*Checks correct arg amount and then checks if the syscall was successful*/
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
         } else {
             result = closeFile(createFile(argv[2],O_RDONLY, 444));
+            /*Close() returns 0 if successful and -1 if failed so checks errors if != 0*/
             if (result != 0) {
                 perror("Error closing the file");
                 return -1;
@@ -64,10 +71,12 @@ int main(int argc, char** argv){
         }
     }
     else if (strcmp(argv[1], "delete") == 0) {
+        /*Checks correct arg amount and then checks if the syscall was successful*/
         if (argc != 3) {
             printf("Invalid amount of arguments. Exiting...\n");
             return -1;
         } else {
+            /*Unlink() returns 0 if successful and -1 if failed so checks errors if != 0*/
             if (deleteFile(argv[2]) != 0) {
                 perror("Error closing file");
                 return -1;
